866151d302fdf8a9b38b68e025ca7888eb3c5745
William Leung <leungwk25@gmail.com>
2017-11-14 13:37:38 -0800

    n = 2

---
+++ n=2/myCRM/myCRM/myCRM/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace myCRM.Models
+{
+    [AddINotifyPropertyChangedInterface]
+    public class Contact 
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+    }
+}
---
+++ n=2/myCRM/myCRM/myCRM/PageModels/ContactListPageModel.cs
@@ -0,0 +1,53 @@
+﻿using Acr.UserDialogs;
+using myCRM.Models;
+using myCRM.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace myCRM.PageModels
+{
+    public class ContactListPageModel : FreshMvvm.FreshBasePageModel
+    {
+        public List<Contact> ContactList { get; set; }
+
+        IDataService _dataService;
+        IUserDialogs _userDialogs;
+
+        public ContactListPageModel (IDataService dataService, IUserDialogs userDialogs)
+        {
+            _dataService = dataService;
+            _userDialogs = userDialogs;
+        }
+
+        public ContactListPageModel()
+        {
+
+        }
+
+        public override async void Init (object initData)
+        {
+            base.Init(initData);
+
+            _userDialogs.ShowLoading();
+
+            ContactList = await _dataService.GetContacts();
+
+            _userDialogs.HideLoading();
+        }
+
+        public Contact SelectedContact
+        {
+            get { return null; }
+            set
+            {
+                CoreMethods.PushPageModel<ContactPageModel>(value);
+                RaisePropertyChanged();
+
+            }
+        }
+    }
+}
